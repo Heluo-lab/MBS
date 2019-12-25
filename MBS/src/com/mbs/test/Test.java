14fc762784152f20166f123056ede500b98e8c11
Heluo-lab <2271436124@qq.com>
2019-12-25 16:51:21 +0800

    Tss

--- MBS/src/com/mbs/test/Test.java
+++ MBS/src/com/mbs/test/Test.java
@@ -1,11 +1,10 @@
 package com.mbs.test;
 
+import java.io.UnsupportedEncodingException;
+
 public class Test {
 	
-	public static void main(String[] args) {
-		System.out.println("Hello Git Git Git");
-		System.out.println("基斯杜父u");
-		System.out.println("dsf");
-		System.out.println("段积鸿");
+	public static void main(String[] args) throws UnsupportedEncodingException {
+		System.out.println(new String("姊﹁姯鑾庢椂灏氬唴琛ｆ棗鑸板簵".getBytes("gbk"),"utf-8"));
 	}
 }
