2bfad2d960b9906c630b833945e5b230f1bbb850
97489 <97489@192.168.43.203>
2020-01-02 11:55:34 +0800

    Service 未完成

---
+++ MBS/yjy/com/mbs/service/ProductService.java
@@ -0,0 +1,12 @@
+package com.mbs.service;
+
+import com.mbs.dao.impl.ProductDaoimpl;
+import com.mbs.dto.IDColorSizeOf;
+import com.mbs.pojo.Repository;
+
+public interface ProductService {
+//	根据商品ID加载内容
+	public IDColorSizeOf LoadingfoGoodsID(int id);
+//	根据商品id颜色尺码查询库存
+	public Repository GidColorSizeOfRepositoryCount(int Gid,String ColorCode,String sizes);
+}
