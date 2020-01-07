ce41088148a8fe41c6127ba12ff49ee92c0d0ffe
Administrator <Administrator@PC-20180512DXJC>
2020-01-07 22:37:21 +0800

    提交了serviceimpl

--- MBS/ljq/com/mbs/service/impl/AccountServiceImpl.java
+++ MBS/ljq/com/mbs/service/impl/AccountServiceImpl.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import com.mbs.dao.AccountDao;
 import com.mbs.dao.impl.AccountDaoImpl;
 import com.mbs.db.DBHelper;
+import com.mbs.pojo.Account;
 import com.mbs.service.AccountService;
 
 public class AccountServiceImpl implements AccountService{
@@ -38,7 +39,7 @@ public class AccountServiceImpl implements AccountService{
 		boolean flag =false;
 		try {
 			conn.setAutoCommit(false);
-			flag =dao.selectAccountEail(email, conn);
+			flag =dao.selectAccountEmail(email, conn);
 			conn.commit();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -51,6 +52,49 @@ public class AccountServiceImpl implements AccountService{
 			DBHelper.release();
 		}
 		return flag;
+	}
+	@Override
+	public boolean insertAccount(Account account) {
+		Connection conn =DBHelper.getConnection();
+		boolean flag =false;
+		try {
+			conn.setAutoCommit(false);
+			flag =dao.insertAccount(account, conn);
+			conn.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			try {
+				conn.rollback();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}finally {
+			DBHelper.release();
+		}
+		return flag;
+	}
+	
+	//登录
+	//查询用户
+	@Override
+	public Account queryAccount(String email) {
+		Connection conn =DBHelper.getConnection();
+		Account account =null;
+		try {
+			conn.setAutoCommit(false);
+			account =dao.selectAccount(email, conn);
+			conn.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			try {
+				conn.rollback();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}finally {
+			DBHelper.release();
+		}
+		return account;
 	}
 
 }
