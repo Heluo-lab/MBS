ffac631a4a1d66f05983ec01332a4f7e3509fc76
Administrator <Administrator@PC-20180512DXJC>
2020-01-07 22:35:34 +0800

    提交dao文件

--- MBS/ljq/com/mbs/dao/AccountDao.java
+++ MBS/ljq/com/mbs/dao/AccountDao.java
@@ -1,11 +1,18 @@
 package com.mbs.dao;
 
+
 import java.sql.Connection;
+
+import com.mbs.pojo.Account;
 
 public interface AccountDao {
 	//查询用户昵称
 	public boolean selectAccountName(String name,Connection conn) throws Exception;
 	//查询用户邮箱
-	public boolean selectAccountEail(String email,Connection conn) throws Exception;
-	
+	public boolean selectAccountEmail(String email,Connection conn) throws Exception;
+	//插入注册用户
+	public boolean insertAccount(Account account,Connection conn) throws Exception;
+	//查询用户(登录)
+	public Account selectAccount(String email,Connection conn) throws Exception;
 }
+
