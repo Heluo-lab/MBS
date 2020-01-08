20063fdedee9993bdf1a6b5c0b655ac48283bd7e
Administrator <Administrator@PC-20180512DXJC>
2020-01-08 22:16:45 +0800

    提交了accountdao

--- MBS/ljq/com/mbs/dao/AccountDao.java
+++ MBS/ljq/com/mbs/dao/AccountDao.java
@@ -12,7 +12,11 @@ public interface AccountDao {
 	public boolean selectAccountEmail(String email,Connection conn) throws Exception;
 	//插入注册用户
 	public boolean insertAccount(Account account,Connection conn) throws Exception;
-	//查询用户(登录)
+	//查询用户(无密码登录)
 	public Account selectAccount(String email,Connection conn) throws Exception;
+	//修改用户密码，临时密码处
+	public void updateAccount(Account account,Connection conn) throws Exception;
+	//查询用户(登录)
+	public Account login(String email,String pwd,Connection conn) throws Exception;
 }
 
