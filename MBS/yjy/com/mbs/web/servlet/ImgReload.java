315c4393606e0cced12801a0957305c62cd32d31
level269 <2737049873@qq.com>
2020-01-09 20:04:34 +0800

    ,,,

--- MBS/yjy/com/mbs/web/servlet/ImgReload.java
+++ MBS/yjy/com/mbs/web/servlet/ImgReload.java
@@ -2,16 +2,14 @@ package com.mbs.web.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.mbs.dao.ProductDao;
 import com.mbs.dao.impl.ProductDaoimpl;
-import com.mbs.dto.IDColorSizeOf;
+@SuppressWarnings("all")
 @WebServlet("/imgreload")
 public class ImgReload extends HttpServlet{
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
