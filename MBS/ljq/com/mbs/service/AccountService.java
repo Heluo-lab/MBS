926e16a793ab9d80dae6a48ce979a93a408ca609
Administrator <Administrator@PC-20180512DXJC>
2020-01-09 12:41:28 +0800

    提交了accountservice

--- MBS/ljq/com/mbs/service/AccountService.java
+++ MBS/ljq/com/mbs/service/AccountService.java
@@ -1,5 +1,7 @@
 package com.mbs.service;
 
+import java.sql.Connection;
+
 import com.mbs.pojo.Account;
 
 public interface AccountService {
@@ -7,8 +9,12 @@ public interface AccountService {
 	public boolean queryAccountName(String name);
 	//查询用户邮箱
 	public boolean queryAccountEmail(String email);
-	//插入注册用户
+	//插入用户
 	public boolean insertAccount(Account account);
-	//查询用户
+	//查询用户,无密码登录
 	public Account queryAccount(String email);
+	//修改用户密码，临时密码处
+	public void updateAccount(Account account);
+	//登录
+	public Account login(String email,String pwd);
 }
