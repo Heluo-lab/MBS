5eb440b46e5dbbb91e12b713366accb9c49ac6a5
Administrator <Administrator@PC-20180512DXJC>
2020-01-09 12:43:53 +0800

    修改了普通登录按钮servlet

--- MBS/ljq/com/mbs/web/servlet/LoginBtnServlet.java
+++ MBS/ljq/com/mbs/web/servlet/LoginBtnServlet.java
@@ -5,6 +5,7 @@ import java.net.URLDecoder;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -31,68 +32,73 @@ public class LoginBtnServlet extends HttpServlet{
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		AccountService service =new AccountServiceImpl();
 		//获取传过来的值
-		//登录类别
-		//String loginsort =req.getParameter("loginsort");
-		//普通登录
-		//if ("ordinary".equals(loginsort)) {
-			//登录邮箱
-			String loginid =req.getParameter("loginid");
-			loginid =URLDecoder.decode(loginid, "UTF-8");
-			//登录密码
-			String loginpwd =req.getParameter("loginpwd");
-			loginpwd =URLDecoder.decode(loginpwd, "UTF-8");
-			//检验是否存在
-			Account account =service.queryAccount(loginid);
-			//如果用户为null
-			if (account==null) {
-				//告诉前台传的是json
-				resp.setCharacterEncoding("UTF-8");
-				resp.setHeader("Content-Type", "application/text;charset=utf-8");
-				String loginMsg ="error";
-				resp.getWriter().print(loginMsg);
-			}else {
-				//该用户存在，判断密码是否正确
-				//获取该用户的密码
-				String pwd =account.getAccountPass();
-				//密码正确
-				if (pwd.equals(loginpwd)) {
-					//将该用户存在session里,方便获取该用户名字
-					req.getSession().setAttribute("account", account);
-					//告诉前台传的是json
-					resp.setCharacterEncoding("UTF-8");
-					resp.setHeader("Content-Type", "application/json;charset=utf-8");
-					JSONObject jo =JSONObject.fromObject(account);
-					resp.getWriter().print(URLDecoder.decode(jo.toString()));
-				}
-				//密码不正确
-				else {
-					//告诉前台传的是json
-					resp.setCharacterEncoding("UTF-8");
-					resp.setHeader("Content-Type", "application/text;charset=utf-8");
-					String loginMsg ="error";
-					resp.getWriter().print(loginMsg);
-				}
+		//获取记住密码
+		String checkedpwd =req.getParameter("checkedpwd");
+		//自动登录
+		String checkedauto =req.getParameter("checkedauto");
+		//登录邮箱
+		String loginid =req.getParameter("loginid");
+		loginid =URLDecoder.decode(loginid, "UTF-8");
+		//登录密码
+		String loginpwd =req.getParameter("loginpwd");
+		loginpwd =URLDecoder.decode(loginpwd, "UTF-8");
+		//检验是否存在
+		Account account =service.login(loginid , loginpwd);
+		//如果用户为null
+		if (account==null) {
+			//告诉前台传的是json
+			resp.setCharacterEncoding("UTF-8");
+			resp.setHeader("Content-Type", "application/text;charset=utf-8");
+			String loginMsg ="error";
+			resp.getWriter().print(loginMsg);
+		}else {
+			//该用户存在
+			//将该用户存在session里,方便获取该用户名字
+			req.getSession().setAttribute("account", account);
+			//存一个标识到session中，用来标识记住密码还是自动登录,还是都没有勾选
+			String loginsymbol="";
+			//判断是否勾选了自动登录
+			if ("true".equals(checkedauto)) {
+				//存loginid到cookie
+				Cookie cookie_loginid =new Cookie("cookie_loginid", loginid);
+				cookie_loginid.setMaxAge(10*60);
+				//存loginpwd到cookie
+				Cookie cookie_loginpwd =new Cookie("cookie_loginpwd", loginpwd);
+				cookie_loginpwd.setMaxAge(10*60);
+				
+				resp.addCookie(cookie_loginid);
+				resp.addCookie(cookie_loginpwd);
+				//存一个标识到session中，用来标识记住密码还是自动登录
+				loginsymbol ="autologin";
+				req.getSession().setAttribute("loginsymbol", loginsymbol);
 			}
-	//	}
-		/*
-		 * //无密码登录 else if ("other".equals(loginsort)) { //登录邮箱 String loginmob
-		 * =req.getParameter("loginmob"); loginmob =URLDecoder.decode(loginmob,
-		 * "UTF-8"); //登录口令 String logincode =req.getParameter("logincode"); logincode
-		 * =URLDecoder.decode(logincode, "UTF-8"); //获取session存的验证码 String servercode
-		 * =req.getSession().getAttribute("emailMsg").toString(); //判断验证码是否正确 if
-		 * (servercode.equals(logincode)) { //检验该用户是否存在 Account account
-		 * =service.queryAccount(loginmob); //用户不存在 if (account==null) { //告诉前台传的是json
-		 * resp.setCharacterEncoding("UTF-8"); resp.setHeader("Content-Type",
-		 * "application/json;charset=utf-8"); String loginMsg ="error";
-		 * resp.getWriter().print(loginMsg); }else { //将该用户存入session中
-		 * req.getSession().setAttribute("account", account.toString()); //告诉前台传的是json
-		 * resp.setCharacterEncoding("UTF-8"); resp.setHeader("Content-Type",
-		 * "application/json;charset=utf-8"); JSONObject jo
-		 * =JSONObject.fromObject(account);
-		 * resp.getWriter().print(URLDecoder.decode(jo.toString())); } }else { //验证码错误
-		 * //告诉前台传的是json resp.setCharacterEncoding("UTF-8");
-		 * resp.setHeader("Content-Type", "application/json;charset=utf-8"); String code
-		 * ="code"; resp.getWriter().print(code); } }
-		 */
+			//判断是否勾选了记住密码,且没有勾选自动登录
+			if ("false".equals(checkedauto) && "true".equals(checkedpwd)) {
+//				//存loginid到cookie
+//				Cookie cookie_loginid =new Cookie("cookie_loginid", loginid);
+//				cookie_loginid.setMaxAge(10*60);
+//				//存loginpwd到cookie
+//				Cookie cookie_loginpwd =new Cookie("cookie_loginpwd", loginpwd);
+//				cookie_loginpwd.setMaxAge(10*60);
+//				
+//				resp.addCookie(cookie_loginid);
+//				resp.addCookie(cookie_loginpwd);
+				
+				//存一个标识到session中，用来标识记住密码还是自动登录
+				loginsymbol ="rempwd";
+				req.getSession().setAttribute("loginsymbol", loginsymbol);
+			}
+			//记住密码，自动登录都没有勾选
+			if ("false".equals(checkedauto) && "false".equals(checkedpwd)) {
+				//存一个标识到session中，用来标识记住密码还是自动登录
+				loginsymbol ="";
+				req.getSession().setAttribute("loginsymbol", loginsymbol);
+			}
+			//告诉前台传的是json
+			resp.setCharacterEncoding("UTF-8");
+			resp.setHeader("Content-Type", "application/json;charset=utf-8");
+			JSONObject jo =JSONObject.fromObject(account);
+			resp.getWriter().print(URLDecoder.decode(jo.toString()));
+		}
 	}
 }
