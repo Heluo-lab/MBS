343bb83cf68d672464b9cc2c57fec5039abed13e
Administrator <Administrator@PC-20180512DXJC>
2020-01-09 14:02:50 +0800

    增加了service层的两个文件users

---
+++ MBS/ljq/com/mbs/service/UsersService.java
@@ -0,0 +1,8 @@
+package com.mbs.service;
+
+import java.sql.Connection;
+
+public interface UsersService {
+	//插入用户
+	public void insertUsers(String usersId);
+}
---
+++ MBS/ljq/com/mbs/service/impl/UsersServiceImpl.java
@@ -0,0 +1,32 @@
+package com.mbs.service.impl;
+
+import java.sql.Connection;
+
+import com.mbs.dao.UsersDao;
+import com.mbs.dao.impl.UsersDaoImpl;
+import com.mbs.db.DBHelper;
+import com.mbs.service.UsersService;
+
+public class UsersServiceImpl implements UsersService{
+	private UsersDao dao =new UsersDaoImpl();
+	//插入用户
+	@Override
+	public void insertUsers(String usersId) {
+		Connection conn =DBHelper.getConnection();
+		try {
+			conn.setAutoCommit(false);
+			dao.insertUsers(usersId, conn);
+			conn.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			try {
+				conn.rollback();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}finally {
+			DBHelper.release();
+		}
+	}
+
+}
